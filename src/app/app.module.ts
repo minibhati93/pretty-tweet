dc1d469e4bb2ed4f9319e50eff495a0f6b83b192
Mini Bhati <mini.bhati@mheducation.com>
2022-01-10 14:22:37 +0530

    Removed tinymce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EditorModule } from '@tinymce/tinymce-angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
@@ -13,7 +12,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    EditorModule,
     FormsModule,
     ReactiveFormsModule
   ],
