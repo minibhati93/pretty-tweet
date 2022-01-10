336374d3740f6c8751a7742d9002aa1fd45c462a
Mini Bhati <mini.bhati@mheducation.com>
2022-01-10 14:21:58 +0530

    Stage 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { toUnicodeVariant } from './unicodeVariant';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pretty-tweet';
+
+  content: string = '';
+  result: string = '';
+
+  constructor(private clipboard: Clipboard) {}
+
+  copyText() {
+    this.clipboard.copy(this.content);
+  }
+
+  format(variant: string) {
+    const word: string | null = this.getSelectedText();
+    let formatWord;
+    if (typeof word === "string") {
+      formatWord = toUnicodeVariant(word, variant, '');
+      this.content = this.content.replace(word, formatWord);
+    }
+  }
+
+  boldWord(): void {
+    const word: string | null = this.getSelectedText();
+    let boldWord;
+    if (typeof word === "string") {
+      boldWord = toUnicodeVariant(word, 'bold', '');
+      this.content = this.content.replace(word, boldWord);
+    }
+  }
+
+  italicWord() {
+    const word: string | null = this.getSelectedText();
+    let italicWord;
+    if (typeof word === "string") {
+      italicWord = toUnicodeVariant(word, 'italic', '');
+      this.content = this.content.replace(word, italicWord);
+    }
+  }
+
+  getSelectedText(): string | null {
+    if (window.getSelection) {  // all browsers, except IE before version 9
+      var range = window.getSelection();
+      return range?.toString() || null;
+    }
+    return null;
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.{html,ts}']
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
