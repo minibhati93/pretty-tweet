18116eb6c21c6c5ff23aae2cb44d4ae773bbda61
Mini Bhati <mini.bhati@mheducation.com>
2022-01-10 17:03:20 +0530

    Format toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormatToolbarComponent } from './format-toolbar/format-toolbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormatToolbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
